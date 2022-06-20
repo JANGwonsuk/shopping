f70355a50711c9c9d01b4dbd86cfaaf4135c5996
JANGwonsuk <jjangga700@naver.com>
2022-06-20 15:44:40 +0900

    Update header.php

--- layouts/header.php
+++ layouts/header.php
@@ -28,7 +28,7 @@ session_start();
     <!--Navbar-->
     <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 fixed-top">
         <div class="container">
-         <img class="logo" src="assets/imgs/logo.jpeg" />
+         <img class="logo" src="assets/imgs/logo.png" />
          <h2 class="brand">Orange</h2>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
