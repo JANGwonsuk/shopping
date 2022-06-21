417a7bdeeab70715dd207835dc05934c2d39bad0
JANGwonsuk <jjangga700@naver.com>
2022-06-22 00:25:26 +0900

    2022.06.22
    00:25

--- single_product.php
+++ single_product.php
@@ -68,7 +68,7 @@ if(isset($_GET['product_id'])){
 
 
             <div class="col-lg-6 col-md-12 col-12">
-                <h6>Men/Shoes</h6>
+                <h6>Everything is sold the moment you think about it</h6>
                 <h3 class="py-4"><?php echo $row['product_name']; ?></h3>
                 <h2>$<?php echo $row['product_price']; ?></h2>
 
